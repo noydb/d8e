5b42c0ac71ba8906f6501d6436f72f54cf129227
benj <bpower@neslotech.co.za>
2024-10-06 20:48:10 +0200

    try fix import error

--- d8e.js
+++ d8e.js
@@ -1,10 +1,9 @@
 const { existsSync, mkdirSync, readFile, writeFile, readdirSync, statSync } = require('fs');
 const { join, dirname, extname, relative } = require('path');
 
-// Use __dirname to resolve local module paths
-const { copyImages } = require(join(__dirname, 'src', 'asset'));
-const { inlineAndMinifyJS, inlineAndMinifyCSS } = require(join(__dirname, 'src', 'inliner'));
-const { minifyHTML } = require(join(__dirname, 'src', 'minify'));
+const { copyImages } = require('./src/asset');
+const { inlineAndMinifyJS, inlineAndMinifyCSS } = require('./src/inliner');
+const { minifyHTML } = require('./src/minify');
 
 const inputDir = process.argv[2];
 if (!inputDir) {
@@ -12,11 +11,9 @@ if (!inputDir) {
   process.exit(1);
 }
 
-// Use current working directory for input and output
 const cwd = process.cwd();
-const fullInputPath = join(cwd, inputDir);
-const outputDir = join(cwd, 'dist');
 
+const outputDir = join(cwd, 'dist');
 if (!existsSync(outputDir)) {
   mkdirSync(outputDir, { recursive: true });
 }
@@ -51,6 +48,8 @@ function processHTMLFile(inputPath, outputPath) {
   });
 }
 
+const fullInputPath = join(cwd, inputDir);
+
 function processDirectory(dir) {
   readdirSync(dir).forEach(file => {
     const fullPath = join(dir, file);
@@ -58,8 +57,7 @@ function processDirectory(dir) {
       processDirectory(fullPath);
     } else if (extname(file).toLowerCase() === '.html') {
       const relativePath = relative(fullInputPath, fullPath);
-      const outputPath = join(outputDir, relativePath);
-      processHTMLFile(fullPath, outputPath);
+      processHTMLFile(fullPath, join(outputDir, relativePath));
     }
   });
 }
